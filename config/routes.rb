1d33a563d0f0e1dee77c14cd1a2130877d423320
macsandro <sandroucb@gmail.com>
2016-04-20 21:51:33 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do  
-  resources :customers do
+  resources :customers do 
+    resources :projects, dependent: :destroy
   end
   devise_for :users
   root "pages#home"
